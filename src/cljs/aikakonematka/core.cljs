ef95048953efe6991592c32c16a4ff4115b45e41
your name <your e-mail>
2018-03-02 15:24:04 +0900

    Display puzzle clear message(only once)

--- src/cljs/aikakonematka/core.cljs
+++ src/cljs/aikakonematka/core.cljs
@@ -22,21 +22,42 @@
   (/ sheet-width btn-sprite-col-num))
 (defn- get-button-height [sheet-height btn-sprite-row-num]
   (/ sheet-height btn-sprite-row-num))
-(defonce game-state (atom {:sprites             {}
-                           :sprites-state       {}
-                           :puzzle-width-height 0
-                           :piece-x-scale       0
-                           :piece-y-scale       0}))
+(defonce game-state (atom {:sprites                {}
+                           :sprites-state          {}
+                           :puzzle-width-height    0
+                           :piece-x-scale          0
+                           :piece-y-scale          0
+                           :puzzle-completion-text nil}))
 
 (def flipped-state "FLIPPED")
 (def non-flipped-state "NON-FLIPPED")
 
 (def game (atom nil))
 
+(defn show-completion-text []
+  (.setTo
+    (.-anchor
+      (.text
+        (.-add @game)
+        (/ (.-innerWidth js/window) 5)
+        (/ (.-innerHeight js/window) 20)
+        "Congrats! \n You made it :D Yeahhhh!"
+        (clj->js {:font            "40px Arial"
+                  :fill            "#06184c"
+                  :backgroundColor "#f7eb7e"
+                  :align           "center"}))) 0.1))
+
+
 (defn puzzle-is-completed []
-  (when (every? #(= non-flipped-state (val %)) (:sprites-state @game-state))
+  (when (and (every? #(= non-flipped-state (val %)) (:sprites-state @game-state))
+             (not (:puzzle-completion-text @game-state)))
     (println "From puzzle-is-completed : " (:sprites-state @game-state))
-    (println "Congrats" "You've got a great start to solving!")))
+    (println "Congrats" "You've got a great start to solving!")
+    (swap!
+      game-state
+      assoc
+      :puzzle-completion-text
+      (show-completion-text))))
 
 (defn- preload []
   (.spritesheet
