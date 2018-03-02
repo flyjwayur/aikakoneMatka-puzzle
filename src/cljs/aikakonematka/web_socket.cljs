dcffaecf2354665eec9bfeaa40b3b3f1b6529f84
your name <your e-mail>
2018-03-02 20:44:44 +0900

    Define multi-methods in top level instead of including them in fn

--- src/cljs/aikakonematka/web_socket.cljs
+++ src/cljs/aikakonematka/web_socket.cljs
@@ -42,40 +42,39 @@
               util/flipped-state)
             (.setTo piece-scale 0 0)))))))
 
-;Initialize event-msg-hanlders for handling different socket events.
-(defn- define-event-msg-handler []                          ; To check the :id key on the msg and route it accordingly.
-  (defmulti event-msg-handler :id)                          ; To initialize it with a map containing fns
-  ; for handshake, state change, and incoming msg.
-  (defmethod event-msg-handler :default [{:keys [event]}]   ; To define a default event-handling fns.
-    (println "Unhandled event: " event))
+;Initialize event-msg-handlers for handling different socket events.
+(defmulti event-msg-handler :id)                            ; To check the :id key on the msg and route it accordingly.
+; for handshake, state change, and incoming msg.          ; To initialize it with a map containing fns
+(defmethod event-msg-handler :default [{:keys [event]}]     ; To define a default event-handling fns.
+  (println "Unhandled event: " event))
 
-  (defmethod event-msg-handler :chsk/state [{:keys [?data]}]
-    (if (= ?data {:first-open? true})
-      (println "Channel socket successfully established!")
-      (println "Channel socket state change:" ?data)))
+(defmethod event-msg-handler :chsk/state [{:keys [?data]}]
+  (if (= ?data {:first-open? true})
+    (println "Channel socket successfully established!")
+    (println "Channel socket state change:" ?data)))
 
-  (defmethod event-msg-handler :chsk/recv [{:keys [?data]}]
-    ;when client received a pushed msg from the server via web socket
-    (let [[event-id event-data] ?data]
-      (println "received " [event-id event-data])
-      (case event-id
-        :aikakone/sprites-state (do
-                                  (synchronize-puzzle-board util/game-state event-data)
-                                  (util/puzzle-is-completed))
-        (println event-id " is unknown event type"))))
+(defmethod event-msg-handler :chsk/recv [{:keys [?data]}]
+  ;when client received a pushed msg from the server via web socket
+  (let [[event-id event-data] ?data]
+    (println "received " [event-id event-data])
+    (case event-id
+      :aikakone/sprites-state (do
+                                (synchronize-puzzle-board util/game-state event-data)
+                                (util/puzzle-is-completed))
+      (println event-id " is unknown event type"))))
 
-  (defn send-uid []
-    (chsk-send! [:aikakone/uid (:uid @util/game-state)]))
+(defn send-uid []
+  (chsk-send! [:aikakone/uid (:uid @util/game-state)]))
 
-  (defn send-sprites-state! [game-state]
-    (println "sending " (:sprites-state @game-state))
-    (chsk-send! [:aikakone/sprites-state (:sprites-state @game-state)]))
+(defn send-sprites-state! [game-state]
+  (println "sending " (:sprites-state @game-state))
+  (chsk-send! [:aikakone/sprites-state (:sprites-state @game-state)]))
 
-  (defmethod event-msg-handler :chsk/handshake [{:keys [?data]}]
-    (let [[?uid ?csrf-token ?handshake-data] ?data]
-      (println "Handshake:" ?data)
-      (swap! util/game-state assoc :uid ?uid)
-      (send-uid))))
+(defmethod event-msg-handler :chsk/handshake [{:keys [?data]}]
+  (let [[?uid ?csrf-token ?handshake-data] ?data]
+    (println "Handshake:" ?data)
+    (swap! util/game-state assoc :uid ?uid)
+    (send-uid)))
 
-(defn start-router []                                  ; To create msg router to handle incoming msg.
-  (sente/start-chsk-router! ch-chsk (define-event-msg-handler))) ; To pass the fn for handling the incoming msg.
+(defn start-router []                                       ; To create msg router to handle incoming msg.
+  (sente/start-chsk-router! ch-chsk event-msg-handler))   ; To pass the fn for handling the incoming msg.
