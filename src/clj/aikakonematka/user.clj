0e570657740f444e48b986d037835cdada9e8869
your name <your e-mail>
2018-03-13 05:20:05 +0900

    Create user ns for starting/stoping server

---
+++ src/clj/aikakonematka/user.clj
@@ -0,0 +1,17 @@
+(ns aikakonematka.user
+  (:require [aikakonematka.core :as core]
+            ))
+
+(defonce server (atom nil))
+
+(defn stop-server []
+  (when-not (nil? server)
+    ;; graceful shutdown; wait 100ms for existing requests to be finished
+    ;; :timeout is optional, when no timeout, stop immediately.
+    (@server :timout 100)
+    (reset! server nil)))
+
+(defn start-server []
+  (reset! server (core/-main)))
+
+
