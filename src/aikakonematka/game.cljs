1b15f387ffc57fcb4f1d70cd008d367c03419fe9
Hye Soo Park <flyjwayur@gmail.com>
2018-06-14 22:37:10 +0300

    Do not make control-button appear, when resizing and not playing

--- src/aikakonematka/game.cljs
+++ src/aikakonematka/game.cljs
@@ -238,8 +238,9 @@
 (defn- on-resize []
   (util/set-puzzle-width-height-in-relation-to-window-size!)
   ;Scale 0 in hide-control-buttons and Tween in show-control-buttons!
-  (util/hide-control-buttons!)
-  (util/show-control-buttons!)
+  (when util/currently-playing-game?
+    (util/hide-control-buttons!)
+    (util/show-control-buttons!))
   (util/synchronize-puzzle-board! (:sprites-state @util/game-state))
   (util/positioning-ui-elements!))
 
