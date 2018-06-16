fddbb00b0bd899e5bf686121ae503a87acf6c1a1
Hye Soo Park <flyjwayur@gmail.com>
2018-06-16 11:35:55 +0300

    check backend health in intro page to awaken sleeping Heroku server
    With backend health check, the game(ex randomizing puzzle pieces, time etc)
    can be ready, when user clicks the game play-button

--- src/aikakonematka/components.cljs
+++ src/aikakonematka/components.cljs
@@ -31,6 +31,10 @@
 (def backend-url
   (str config/protocol-to-backend "://" config/backend-host))
 
+(defn- check-backend-health []
+  (go (let [response (<! (http/get (str backend-url "/health")))]
+        (println :backend-health-check-success (:body response)))))
+
 ;- view functions & definition -
 
 (defn go-back-to-game-button []
@@ -118,46 +122,48 @@
          util/puzzle-images)))
 
 (def game-intro-view
-  [:div
-   [:img {:style    {:position                  "fixed"
-                     :width                     "60%"
-                     :height                    "55%"
-                     :z-index                   "6"
-                     :right                     "30%"
-                     :bottom                    "30%"
-                     :animation-name            "titleAnimation"
-                     :transform                 "rotateX(40deg)"
-                     :animation-duration        "2s"
-                     :animation-iteration-count "infinite"
-                     :animation-direction       "alternate"}
-          :src      "images/intro-title.png"
-          :width    "100%"
-          :height   "100%"
-          :on-click util/show-puzzle-selection!}]
-   [:img {:style    {:position                  "fixed"
-                     :z-index                   "5"
-                     :width                     "20%"
-                     :height                    "20%"
-                     :right                     "10%"
-                     :animation-name            "clicktostart"
-                     :animation-duration        "2s"
-                     :animation-iteration-count "infinite"
-                     :animation-direction       "alternate"}
-          :src      "images/click-to-start-button.png"
-          :on-click util/show-puzzle-selection!}]
-   [:picture {:style    {:position "absolute"
-                         :z-index  "4"
-                         :width    "100%"
-                         :height   "100%"}
-              :on-click util/show-puzzle-selection!}
-    [:source {:media  "(min-width: 600px)"
-              :srcSet "images/aikakone-intro.jpg"}]
-    [:img {:src    "images/aikakone-intro-mobile.jpg"
-           :alt    "aikakone intro image"
-           :width  "100%"
-           :height "100%"}]]
-   [:div {:style {:display "none"}}
-    [puzzle-selection-view]]])
+  (do
+    (check-backend-health)
+    [:div
+     [:img {:style    {:position                  "fixed"
+                       :width                     "60%"
+                       :height                    "55%"
+                       :z-index                   "6"
+                       :right                     "30%"
+                       :bottom                    "30%"
+                       :animation-name            "titleAnimation"
+                       :transform                 "rotateX(40deg)"
+                       :animation-duration        "2s"
+                       :animation-iteration-count "infinite"
+                       :animation-direction       "alternate"}
+            :src      "images/intro-title.png"
+            :width    "100%"
+            :height   "100%"
+            :on-click util/show-puzzle-selection!}]
+     [:img {:style    {:position                  "fixed"
+                       :z-index                   "5"
+                       :width                     "20%"
+                       :height                    "20%"
+                       :right                     "10%"
+                       :animation-name            "clicktostart"
+                       :animation-duration        "2s"
+                       :animation-iteration-count "infinite"
+                       :animation-direction       "alternate"}
+            :src      "images/click-to-start-button.png"
+            :on-click util/show-puzzle-selection!}]
+     [:picture {:style    {:position "absolute"
+                           :z-index  "4"
+                           :width    "100%"
+                           :height   "100%"}
+                :on-click util/show-puzzle-selection!}
+      [:source {:media  "(min-width: 600px)"
+                :srcSet "images/aikakone-intro.jpg"}]
+      [:img {:src    "images/aikakone-intro-mobile.jpg"
+             :alt    "aikakone intro image"
+             :width  "100%"
+             :height "100%"}]]
+     [:div {:style {:display "none"}}
+      [puzzle-selection-view]]]))
 
 (defn game-screen [search-word->game-img-url game-img]
   (r/create-class
